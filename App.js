4ec0ba079f2b39428b69171c2882944e39411133
Alberto <aeud@hotmail.com>
2020-02-17 16:03:25 -0300

    Inicio de La Pagina

---
+++ App.js
@@ -0,0 +1,25 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express(); //instancia de express
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public")); //Se utiliza para Cargar los Archivos staticos locales
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/Index.html");//
+});
+
+app.post("/", function(req, res) {
+
+  var nombre = req.body.Nombre;
+  var apellido = req.body.Apellido;
+  var mail = req.body.Email;
+
+  console.log(nombre, apellido, mail);
+});
+
+app.listen(3000, function() {
+  console.log("Servidor Corriendo en el Puerto 3000");
+});
